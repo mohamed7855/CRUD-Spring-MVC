023ab29b4eab9e0740ed3962e4b71b3d85c0607f
Mohamed Omara <martm7726@gmail.com>
2023-05-23 18:52:49 +0300

    get data from DB

--- spring-mvc-crud-employee/src/main/java/com/project/springboot/cruddemo/rest/EmployeeRestController.java
+++ spring-mvc-crud-employee/src/main/java/com/project/springboot/cruddemo/rest/EmployeeRestController.java
@@ -15,32 +15,19 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeRestController {
 
-
-    // load employee data
-    private List<Employee> theEmployees;
-
-    @PostConstruct
-    private void loadData() {
-
-        // create employees
-        Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
-        Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
-        Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
-
-        // create the list
-        theEmployees = new ArrayList<>();
-
-        // add to the list
-        theEmployees.add(emp1);
-        theEmployees.add(emp2);
-        theEmployees.add(emp3);
+    private EmployeeService employeeService;
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService){
+        this.employeeService=theEmployeeService;
     }
 
-    // add mapping for "/list"
 
+
+    // add mapping for "/list"
     @GetMapping("/list")
     public String listEmployees(Model theModel) {
 
+        List<Employee> theEmployees = employeeService.findAll();
         // add to the spring model
         theModel.addAttribute("employees", theEmployees);
 
